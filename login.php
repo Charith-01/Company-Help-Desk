ec419c26e8cbe0d2433379d6e676ca46f5464a42
Charith Lakshan <it23841086@my.sliit.lk>
2024-12-04 12:10:45 +0530

    Changed

--- login.php
+++ login.php
@@ -55,7 +55,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - IT Support Desk</title>
+    <title>Login</title>
     <link rel="stylesheet" href="CSS/login.css">
 </head>
 <body>
